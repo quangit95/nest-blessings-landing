c41dea68853bc57e85ef87c8c7007471f1f77b97
QATech <08quang@gmail.com>
2025-08-01 15:19:11 +0700

    day code len lan 2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,7 +25,7 @@ const Footer = () => {
             <div className="space-y-3">
               <div className="flex items-center space-x-3">
                 <MapPin className="w-5 h-5 text-brown-300" />
-                <span className="text-brown-00">{t('footer.address')}</span>
+                <span className="text-brown-200">{t('footer.address')}</span>
               </div>
               <div className="flex items-center space-x-3">
                 <Phone className="w-5 h-5 text-brown-300" />
